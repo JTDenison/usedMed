e79e2d9572c98ef1c78bddbeb13f26ca8bcc10ba
JTDenison <joshd_84@ymail.com>
2017-08-19 23:28:51 -0400

    Add files via upload

--- header.php
+++ header.php
@@ -2,8 +2,12 @@
 <html>
 <head>
 <meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
 	<title>UsedMed</title>
 	<?php wp_head(); ?>
 
 </head>
-<body>
+<body <?php body_class(); ?>>
+
+<?php wp_nav_menu(array('theme_location'=>'primary')); ?>
--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php get_header(); ?>
 
-<h1>index php</h1>
-<?php get_content(); ?>
+<?php 
+	if( have_posts() ):
+		while( have_posts() ): the_post(); ?>
+
+			<h3> <?php the_title(); ?> </h3>
+
+			<p><?php the_content(); ?> </p>
+		
+		<?php endwhile;
+		
+	endif; 
+
+	?>
 
 <?php get_footer(); ?>
