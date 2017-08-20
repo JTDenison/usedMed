9adf2acd0c62610cc1f6b5b6d518c086684b49af
unknown <joshua.denison2020@gmail.com>
2017-08-20 01:38:50 -0400

    last fuck up for tonight

--- header.php
+++ header.php
@@ -8,18 +8,4 @@
 	<?php wp_head(); ?>
 
 </head>
-<body <?php body_class(); ?>>
-
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
-	<title>UsedMed</title>
-	<?php wp_head(); ?>
-
-</head>
-<body <?php body_class(); ?>>
-
-<?php wp_nav_menu(array('theme_location'=>'primary')); ?>
+<body>
