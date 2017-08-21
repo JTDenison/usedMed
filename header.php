0ca06bbfd9bd424d89f8562f8906353c8b8a5137
unknown <joshua.denison2020@gmail.com>
2017-08-21 14:13:52 -0400

    header

--- header.php
+++ header.php
@@ -4,12 +4,20 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
+<meta name="description" content="UsedMed.com is a medical and hospital equipment classified advertising site for new and used medical equipment for sale or wanted, 
+        we also list refurbished medical equipment. We also offer a medical equipment related Services Directory.">
+<meta name="keywords" content="used medical equipment, refurbished, medical, mammography, laproscopy, laboratory, imaging, hospital equipment, gastroscope, 
+        equipment, ENT, endoscopy, endoscopic, EKG, ECG, doctor, dialysis, diagnostic, dental, defibrillator, dealers, CT scanner, centrifuge, c-arm, cardiology, 
+        bone-densitometer, biomedical, bed, autoclave, anesthesiology, anesthesia, x-ray, veterinary, ultrasound, surgical, surgery, sterilizer, scanner, sales, 
+        respiratory, reseller, remarketer, rehabilitation, radiology, physicians office, physical therapy, photographic, pharmacy, orthopedics, ophthalmology, 
+        operating room, OBGYN, nuclear, neurology, neonatal, microscope">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 	<title>UsedMed</title>
 	<?php wp_head(); ?>
 
 </head>
 	<?php
-	// body _class taks array of params body_class(array('class','class'))
+	// body_class takes array of params = body_class(array('class','class'))
 	?>
 <body <?php body_class(); ?>>
 <?php wp_nav_menu(array('theme_location'=>'primary')); ?>
