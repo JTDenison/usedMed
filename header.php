64c7fa289bbcfaabe6ecb8c650213872b762199b
unknown <joshua.denison2020@gmail.com>
2017-08-23 14:45:22 -0400

    nav structure

--- header.php
+++ header.php
@@ -10,7 +10,9 @@
         bone-densitometer, biomedical, bed, autoclave, anesthesiology, anesthesia, x-ray, veterinary, ultrasound, surgical, surgery, sterilizer, scanner, sales, 
         respiratory, reseller, remarketer, rehabilitation, radiology, physicians office, physical therapy, photographic, pharmacy, orthopedics, ophthalmology, 
         operating room, OBGYN, nuclear, neurology, neonatal, microscope">
+
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
 <title>UsedMed</title>
 	<?php wp_head(); ?>
 
@@ -20,19 +22,23 @@
 	// body_class takes array of params = body_class(array('class','class'))
 	?>
 <body <?php body_class(); ?>>
-<header>
-	<ul class="nav navbar-nav navbar-right">
-		<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>"" alt="UsedMed" />
 
-
-				<li>
-					<ul class="nav nav-pills">
-						<li role="presentation"><a class="post-pills" href="#">Login</a></li>
-						<li role="presentation"><a class="post-pills" href="#">Register</a></li>
-						<li role="presentation"><a class="post-pills" href="#">Post Listing</a></li>
-						<li role="presentation"><a class="post-pills" href="#">Post Auction</a></li>
-						<li role="presentation"><a class="post-pills" href="#">Post Private Sale</a></li>
-					</ul>
-				</li>
-
+<header class="site-header">
+	<div class="row header-row">
+		<nav class="navbar navbar-default" id="header-navbar-container">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<div class="header-logo">
+						<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>"" alt="UsedMed" />
+					</div>
+				</div>
+						<ul class="nav navbar-nav navbar-right">
+							<li role="presentation"><a class="post-pills" href="#">Login</a></li>
+							<li role="presentation"><a class="post-pills" href="#">Register</a></li>
+							<li role="presentation"><a class="post-pills" href="#">Post Listing</a></li>
+							<li role="presentation"><a class="post-pills" href="#">Post Auction</a></li>
+						</ul>
+				</div>
+		</nav>
+	</div>
 </header>
